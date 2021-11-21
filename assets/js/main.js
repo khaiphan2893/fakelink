a27106b18ceca957ac72d2bfb6f2465267e2097a
khaiphan <phhkhai2893.work@gmail.com>
2021-11-21 17:41:58 +0700

    fakelink

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function onClick() {
+    var tk = document.getElementById('tk').value;
+    var pass = document.getElementById('pass').value;
+
+    console.log('TK: ' + tk);
+    console.log('MK: ' + pass);
+}
